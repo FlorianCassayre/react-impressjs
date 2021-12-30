d5cbb415052375bf8abee136bf6927c25fd66498
Florian Cassayre <florian.cassayre@gmail.com>
2021-12-30 10:18:01 +0100

    Listen for any key

--- src/components/Impress.js
+++ src/components/Impress.js
@@ -90,31 +90,11 @@ export default class Impress extends Component {
 
     // Listener for keyboard event
     document.addEventListener('keyup', throttle((e) => {
-      if (e.keyCode === 9 ||
-          (e.keyCode >= 32 && e.keyCode <= 40)) {
-        switch (e.keyCode) {
-          case 35: // End
-            this.end();
-            break;
-          case 36: // Home
-            this.home();
-            break;
-          case 33: // Page up
-          case 37: // Left
-          case 38: // Up
-            this.prev();
-            break;
-          case 9:  // Tab
-          case 32: // Space
-          case 34: // Page down
-          case 39: // Right
-          case 40: // Down
-            this.next();
-            break;
-          default:
-            break;
-        }
-      }
+      this.next();
+    }, 250), false);
+
+    document.addEventListener('mousedown', throttle((e) => {
+      this.next();
     }, 250), false);
 
     // Window resize
